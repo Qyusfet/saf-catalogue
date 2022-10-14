3bbaf3e079d8a42fd9dbc768357f6b425faafa3d
Qyusfet <qyusfet@gmail.com>
2022-10-14 21:35:26 +0100

    project first commit stage

---
+++ js/product.js
@@ -0,0 +1,19 @@
+const products = document.querySelectorAll('.products')
+const searchProduct = document.querySelector('#searchProduct')
+
+const displayProducts = (values) =>{
+    products.forEach(element =>{
+        element.style.display = "none"
+        const productName = element.querySelector('h2').innerHTML.toUpperCase();
+        const productDesc = element.querySelector('p').innerHTML.toUpperCase();
+
+
+        if (productName.includes(values) || productDesc.includes(values)){
+            element.style.display = "block"
+        }
+    })
+}
+
+searchProduct.addEventListener('input', (e) =>{
+    displayProducts(e.target.value.toUpperCase())
+})
