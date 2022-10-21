e53078fa4fa155103067eba836a358503d90b6da
Qyusfet <qyusfet@gmail.com>
2022-10-21 02:38:05 +0100

    update

---
+++ js/search1.js
@@ -0,0 +1,25 @@
+const products = document.querySelectorAll('.products')
+// const searchProduct = document.querySelector('#searchProduct')
+
+const displayProducts = (values) =>{
+    products.forEach(element =>{
+        element.style.display = "none"
+        const productName = element.querySelector('h2').innerHTML.toUpperCase();
+        const productDesc = element.querySelector('p').innerHTML.toUpperCase();
+
+
+        if (productName.includes(values) || productDesc.includes(values)){
+            element.style.display = "block"
+
+            alert(values)
+            
+        }
+    })
+}
+
+searchProduct.addEventListener('input', (e) =>{
+    displayProducts(e.target.value.toUpperCase())
+
+    
+})
+
