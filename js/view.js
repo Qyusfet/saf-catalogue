45b1c68bf2c4a2027e1c1d6fcb6ba26c248b5cd0
Quadri Yusuf <qyusfet@gmail.com>
2022-11-22 17:09:51 +0100

    update

--- js/view.js
+++ js/view.js
@@ -2,10 +2,13 @@ const viewAll = document.querySelector('.ViewAll')
 const individual = document.querySelector('.Individuals')
 const corporate = document.querySelector('.Corporate')
 const investment = document.querySelector('.Investments')
+const financing = document.querySelector('.Financing')
+
+viewAll.style = 'background-color: #DD5353'
+viewAll.style.color = 'white'
 
 all = () => 
 {
-alert ('all selected')
 viewAll.style = 'font-weight: 700'
 viewAll.disabled = true
 viewAll.style = 'background-color: #DD5353'
@@ -26,6 +29,11 @@ investment.style.color = 'black'
 investment.style = 'font-weight: 700'
 investment.disabled = false
 
+financing.style = 'background-color: white'
+financing.style.color = 'black'
+financing.style = 'font-weight: 700'
+financing.disabled = false
+
 document.querySelector('#all-product').style.display = 'block'
 document.querySelector('#individual').style.display = 'none'
 document.querySelector('#corporate').style.display = 'none'
@@ -37,7 +45,6 @@ viewAll.addEventListener("click", all);
 
 individuals = () => 
 {
-alert ('individual selected')
 individual.style = 'font-weight: 700'
 individual.disabled = true
 individual.style = 'background-color: #DD5353'
@@ -58,6 +65,11 @@ investment.style.color = 'black'
 investment.style = 'font-weight: 700'
 investment.disabled = false
 
+financing.style = 'background-color: white'
+financing.style.color = 'black'
+financing.style = 'font-weight: 700'
+financing.disabled = false
+
 document.querySelector('#all-product').style.display = 'none'
 document.querySelector('#individual').style.display = 'block'
 document.querySelector('#corporate').style.display = 'none'
@@ -69,7 +81,6 @@ individual.addEventListener("click", individuals);
 
 corporates = () => 
 {
-    alert ('corporate selected')
 corporate.style = 'font-weight: 700'
 corporate.disabled = true
 corporate.style = 'background-color: #DD5353'
@@ -90,6 +101,11 @@ investment.style.color = 'black'
 investment.style = 'font-weight: 700'
 investment.disabled = false
 
+financing.style = 'background-color: white'
+financing.style.color = 'black'
+financing.style = 'font-weight: 700'
+financing.disabled = false
+
 document.querySelector('#all-product').style.display = 'none'
 document.querySelector('#individual').style.display = 'none'
 document.querySelector('#corporate').style.display = 'block'
@@ -101,7 +117,6 @@ corporate.addEventListener("click", corporates);
 
 investments = () => 
 {
-    alert ('investment selected')
 investment.style = 'font-weight: 700'
 investment.disabled = true
 investment.style = 'background-color: #DD5353'
@@ -122,6 +137,11 @@ viewAll.style.color = 'black'
 viewAll.style = 'font-weight: 700'
 viewAll.disabled = false
 
+financing.style = 'background-color: white'
+financing.style.color = 'black'
+financing.style = 'font-weight: 700'
+financing.disabled = false
+
 document.querySelector('#all-product').style.display = 'none'
 document.querySelector('#individual').style.display = 'none'
 document.querySelector('#corporate').style.display = 'none'
@@ -129,3 +149,39 @@ document.querySelector('#investment').style.display = 'block'
 document.querySelector('#financing').style.display = 'none'
 }
 investment.addEventListener("click", investments);
+
+
+financings = () => 
+{
+financing.style = 'font-weight: 700'
+financing.disabled = false
+financing.style = 'background-color: #DD5353'
+financing.style.color = 'white'
+
+investment.style = 'font-weight: 700'
+investment.disabled = true
+investment.style = 'background-color: white'
+investment.style.color = 'black'
+
+individual.style = 'background-color: white'
+individual.style.color = 'black'
+individual.style = 'font-weight: 700'
+individual.disabled = false
+
+corporate.style = 'background-color: white'
+corporate.style.color = 'black'
+corporate.style = 'font-weight: 700'
+corporate.disabled = false
+
+viewAll.style = 'background-color: white'
+viewAll.style.color = 'black'
+viewAll.style = 'font-weight: 700'
+viewAll.disabled = false
+
+document.querySelector('#all-product').style.display = 'none'
+document.querySelector('#individual').style.display = 'none'
+document.querySelector('#corporate').style.display = 'none'
+document.querySelector('#investment').style.display = 'none'
+document.querySelector('#financing').style.display = 'block'
+}
+financing.addEventListener("click", financings);
